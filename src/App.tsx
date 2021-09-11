21ce5e707243ae7774ce9aa03eb19a332be48bb6
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-09-11 16:53:35 +0300

    add Filter + Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import {TodoList} from "./TodoList";
 
+export type filterType = "All" | "Active" | "Completed"
+
 const App =() => {
 
-    const [tasks, setTasks] = useState([
+    let [tasks, setTasks] = useState([
         {id: 1, title: "Bread", checked: true},
         {id: 2, title: "Milk", checked: true},
         {id: 3, title: "Oil", checked: false},
@@ -18,17 +20,42 @@ const App =() => {
     //в Todolist.tsx этой функции в пропсах нужно тоже получить аргумент, но там id у нас отфильтровано
     //фильтр, отрисуй мне все айдишки кроме той которая в тебя пришла
     //запустить перерисовку можем только с помощью сетТаскс через юзСтейт
+
+    //про фильтр: делетеТаск1 это результирующий массив который удовлетворяет условию
     const deleteTask = (taskId: number) => {
        let deleteTask1 = tasks.filter( f => f.id!==taskId )
         setTasks(deleteTask1)
     }
 
 
+    //создали стейт чтобы хранить разные фильтры
+    //написали типизацию для стейта - какого типа значения в него будут приходить (мутный момент)
+    //эта типизация понадобиться когда значение(value) будет приходить в кач-ве аргумента в функцию
+    //прописали логику: новый массив = профильтрованный под разными условиями старый массив
+    //создали функцию, которую передали в пропсах и повесили на кнопки
+    //прописали этой функции логику
+    //кинули эту функцию в сетФильтр
+
+    let [filter, setFilter] = useState<filterType>("All")
+    const changeFilter = (filterValue: filterType) => {
+        setFilter(filterValue)
+    }
+
+    let filtredTasks = tasks
+    if (filter === "Active") {
+        filtredTasks = tasks.filter(f =>!f.checked)
+    }
+    if (filter === "Completed") {
+        filtredTasks = tasks.filter(f =>f.checked)
+    }
+
+
     return <div>
         <TodoList
             title="What to buy"
-            tasks={tasks}
+            tasks={filtredTasks}
             deleteTask={deleteTask}
+            changeFilter={changeFilter}
         />
     </div>
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {filterType} from "./App";
+import {Button} from "./Components/Button";
 
 type tasksPropsType = {
     id: number,
@@ -7,9 +9,11 @@ type tasksPropsType = {
 }
 
 type propsType = {
+    //title=!string — значит что тайтл может прийти а может не прийти в компоненту
     title: string
     tasks: Array<tasksPropsType>
-    deleteTask: (taskId: number) => void
+    deleteTask: (taskId: number) => void //void потому что в функции нет return
+    changeFilter: (filterValue: filterType) => void
 }
 
 export const TodoList = (props:propsType) => {
@@ -23,7 +27,6 @@ export const TodoList = (props:propsType) => {
         <div>
             <ul>
                 {props.tasks.map(m => {
-                    // debugger
                     return (
                         <li key={m.id}>
                             <button onClick={() => props.deleteTask(m.id)}>x</button>
@@ -37,9 +40,11 @@ export const TodoList = (props:propsType) => {
         </div>
 
 
-        <button>Active</button>
-        <button>Completed</button>
-        <button>All</button>
+        <button onClick={() => props.changeFilter('Active')}>Active</button>
+        <button onClick={() => props.changeFilter('Completed')}>Completed</button>
+            <Button changeFilter={props.changeFilter}/>
+            {/*у компоненты не может быть onClick*/}
+        {/*<button onClick={() => props.changeFilter('All')}>All</button>*/}
 
     </div>
     )
