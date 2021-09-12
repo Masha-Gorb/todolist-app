fc621e5a10ccd70f7e904d316c21456c699b4751
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-09-12 09:13:09 +0300

    add filter to additional component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {TodoList} from "./TodoList";
 import {Grosery} from "./Components/Grocery";
 
 export type filterType = "All" | "Active" | "Completed"
+export type filterForProductsType = 'All' | 'Vegetables' | 'Fruits'
 
 const App =() => {
 
@@ -16,13 +17,13 @@ const App =() => {
     ])
 
     let [products, setProduct] = useState([
-            {id: 1, title: 'Potato', isBought: true},
-            {id: 2, title: 'Tomato', isBought: true},
-            {id: 3, title: 'Apple', isBought: false},
-            {id: 4, title: 'Orange', isBought: true},
-            {id: 6, title: 'Onion', isBought: false},
-            {id: 7, title: 'Cabbage', isBought: false},
-            {id: 8, title: 'Grape', isBought: true},
+            {id: 1, title: 'Potato', isVegetable: true},
+            {id: 2, title: 'Tomato', isVegetable: true},
+            {id: 3, title: 'Apple', isVegetable: false},
+            {id: 4, title: 'Orange', isVegetable: false},
+            {id: 6, title: 'Onion', isVegetable: true},
+            {id: 7, title: 'Cabbage', isVegetable: true},
+            {id: 8, title: 'Grape', isVegetable: false},
 
         ]
     )
@@ -40,46 +41,63 @@ const App =() => {
     //запустить перерисовку можем только с помощью сетТаскс через юзСтейт
 
     //про фильтр: делетеТаск1 это результирующий массив который удовлетворяет условию
-    const deleteTask = (taskId: number) => {
-       let deleteTask1 = tasks.filter( f => f.id!==taskId )
-        setTasks(deleteTask1)
+    // const deleteTask = (taskId: number) => {
+    //    let deleteTask1 = tasks.filter( f => f.id!==taskId )
+    //     setTasks(deleteTask1)
+    // }
+
+    let [filter, setFilter] = useState<filterForProductsType>('All')
+    const changeProductFilter = (filterProductsValue: filterForProductsType) => {
+        setFilter(filterProductsValue)
+    }
+
+    let filtredProducts = products
+    if (filter === 'Vegetables') {
+        filtredProducts= products.filter(f => f.isVegetable)
+    }
+    if (filter === 'Fruits') {
+        filtredProducts = products.filter(f => !f.isVegetable)
     }
 
 
     //создали стейт чтобы хранить разные фильтры
     //написали типизацию для стейта - какого типа значения в него будут приходить (мутный момент)
     //эта типизация понадобиться когда значение(value) будет приходить в кач-ве аргумента в функцию
+    //создаем функцию сменитьФильтр - она получает фильтрЗначение определенного типа и вызывает перерисовку стейта (сетФильтр)
+    //в сетФильтр приходит ЗНАЧЕНИЕ ФИЛЬТРА
     //прописали логику: новый массив = профильтрованный под разными условиями старый массив
+    //условия - если фильтр === такому-то значению, то новый массив равно отфильтрованый старый масив с таким то условием
     //создали функцию, которую передали в пропсах и повесили на кнопки
     //прописали этой функции логику
     //кинули эту функцию в сетФильтр
 
-    let [filter, setFilter] = useState<filterType>("All")
-    const changeFilter = (filterValue: filterType) => {
-        setFilter(filterValue)
-    }
-
-    let filtredTasks = tasks
-    if (filter === "Active") {
-        filtredTasks = tasks.filter(f =>!f.checked)
-    }
-    if (filter === "Completed") {
-        filtredTasks = tasks.filter(f =>f.checked)
-    }
+    // let [filter, setFilter] = useState<filterType>("All")
+    // const changeFilter = (filterValue: filterType) => {
+    //     setFilter(filterValue)
+    // }
+    //
+    // let filtredTasks = tasks
+    // if (filter === "Active") {
+    //     filtredTasks = tasks.filter(f =>!f.checked)
+    // }
+    // if (filter === "Completed") {
+    //     filtredTasks = tasks.filter(f =>f.checked)
+    // }
 
 
     return <div>
-        <TodoList
-            title="What to buy"
-            tasks={filtredTasks}
-            deleteTask={deleteTask}
-            changeFilter={changeFilter}
-        />
+        {/*<TodoList*/}
+        {/*    title="What to buy"*/}
+        {/*    tasks={filtredTasks}*/}
+        {/*    deleteTask={deleteTask}*/}
+        {/*    changeFilter={changeFilter}*/}
+        {/*/>*/}
 
         <Grosery
             name={'Healthy list'}
-        product={products}
+        product={filtredProducts}
             deleteProduct={deleteProduct}
+            changeProductFilter={changeProductFilter}
         />
     </div>
 }
--- src/Components/Grocery.tsx
+++ src/Components/Grocery.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import {filterForProductsType} from "../App";
 
 type productsPropsType = {
     id: number
     title: string
-    isBought: boolean
+    isVegetable: boolean
 }
 
 type PropsType = {
     product: Array<productsPropsType>
     name: string
     deleteProduct: (productId: number) => void
+    changeProductFilter: (filterProductsValue: filterForProductsType) => void
 }
 
 
@@ -23,16 +25,18 @@ export const Grosery = (props: PropsType) => {
                     return (
                         <li key={p.id}>
                             <button onClick={() => props.deleteProduct(p.id)}>x</button>
-                            <input type="checkbox" checked={p.isBought}/>
+                            <input type="checkbox" checked={p.isVegetable}/>
                             <span>{p.title}</span>
                         </li>
                     )
                 })}
             </ul>
 
-                    <button>All</button>
-                    <button>Vegetables</button>
-                    <button>Fruits</button>
+
+
+                    <button onClick={() => props.changeProductFilter('All')}>All</button>
+                    <button onClick={() => props.changeProductFilter('Vegetables')}>Vegetables</button>
+                    <button onClick={() => props.changeProductFilter('Fruits')}>Fruits</button>
                 </div>
                 )
                 }
