2d60f6c94a44417c1eb878267a4b602d05dcb55c
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-09-13 19:54:27 +0300

    addTask p.2

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import {filterType} from "./App";
 import {Button} from "./Components/Button";
 
@@ -14,45 +14,74 @@ type propsType = {
     tasks: Array<tasksPropsType>
     deleteTask: (taskId: string) => void //void потому что в функции нет return
     changeFilter: (filterValue: filterType) => void
-    addTask: () => void
+    addTask: (newTaskTitle: string) => void
 }
 
-export const TodoList = (props:propsType) => {
+export const TodoList = (props: propsType) => {
 
+    let [newTaskTitle, setNewTaskTitle] = useState('')
     const addTaskHandler = () => {
-        props.addTask()
+        props.addTask(newTaskTitle)
+    }
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitle(event.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            props.addTask(newTaskTitle)
+            setNewTaskTitle('')
+        }
+    }
+
+    const generalChangeFilter = (filterValue: filterType) => {
+        props.changeFilter(filterValue)
+    }
+    // const onClickChangeFilterActive = () => {
+    //     props.changeFilter('Active')
+    // }
+    // const onClickChangeFilterCompleted = () => {
+    //     props.changeFilter('Completed')
+    // }
+
+    const onChangeDeleteTaskHandler = (id: string) => {
+        props.deleteTask(id)
     }
 
     return (
         <div>
-        <h2>{props.title}</h2>
-        <input/>
+            <h2>{props.title}</h2>
+            <input value={newTaskTitle} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
 
-        <button onClick={addTaskHandler}>+</button>
+            <button onClick={addTaskHandler}>+</button>
             {/*не забывать вызывать функцию кога тут пишем*/}
 
-        <div>
-            <ul>
-                {props.tasks.map(m => {
-                    return (
-                        <li key={m.id}>
-                            <button onClick={() => props.deleteTask(m.id)}>x</button>
-                            <input type="checkbox" checked={m.checked}/>
-                            <span>{m.title}</span>
-                        </li>
-                    )
-                }
-                )}
-            </ul>
-        </div>
+            <div>
+                <ul>
+                    {props.tasks.map(m => {
+
+                        // const onChangeDeleteTaskHandler = () => {
+                        //     props.deleteTask(m.id)
+                        // }
+                            return (
+                                <li key={m.id}>
+                                    <button onClick={ () => onChangeDeleteTaskHandler(m.id)}>x</button>
+                                    <input type="checkbox" checked={m.checked}/>
+                                    <span>{m.title}</span>
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+            </div>
 
 
-        <button onClick={() => props.changeFilter('Active')}>Active</button>
-        <button onClick={() => props.changeFilter('Completed')}>Completed</button>
+            <button onClick={()=>generalChangeFilter('Active')}>Active</button>
+            <button onClick={()=>generalChangeFilter('Completed')}>Completed</button>
             <Button changeFilter={props.changeFilter}/>
             {/*у компоненты не может быть onClick*/}
-        {/*<button onClick={() => props.changeFilter('All')}>All</button>*/}
+            {/*<button onClick={() => props.changeFilter('All')}>All</button>*/}
 
-    </div>
+        </div>
     )
 }
