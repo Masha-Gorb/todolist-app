16fd90992fa47ffdfaf20df738884b075e7502e4
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-10-03 19:30:31 +0300

    refactor tests

--- src/BLL/01/01.test.ts
+++ src/BLL/01/01.test.ts
@@ -1,22 +1,17 @@
 import {multiply, sum} from "./01";
 
-test( 'sum should be correct', () => {
-    //подготовительные данные
-    const a = 1;
-    const b = 2;
-    const c = 5;
+//подготовительные данные вынесли за тесты
+const a = 4;
+const b = 8;
 
+test( 'sum should be correct', () => {
     //действие
     const result = sum(a,b)
-
     //что ожидаем от действия
-    expect(result).toBe(3)
+    expect(result).toBe(12)
 })
 
 test( 'multiply should be correct', ()=> {
-    const a = 4;
-    const b = 8;
     const result2 = multiply(a, b)
     expect(result2).toBe(32)
-
 })
