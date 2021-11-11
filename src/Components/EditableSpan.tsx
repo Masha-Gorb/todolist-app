f2d39780f93800d00703e174948af0a4db747d18
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-11-11 15:13:17 +0300

    update EditableSpan

--- src/Components/EditableSpan.tsx
+++ src/Components/EditableSpan.tsx
@@ -1,26 +1,40 @@
-import React, {KeyboardEvent, useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import styles from "../App.module.css";
 
 export type PropsType = {
     checked: boolean
     title: string
+    // callBack: (todolistId: string, newTaskTitle: string) => void
+
 }
 
 export const EditableSpan = (props: PropsType) => {
     const[edit, setEdit] = useState(false)
+    let [newTaskTitle, setNewTaskTitle] = useState(props.title)
+
+    console.log(newTaskTitle)
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitle(event.currentTarget.value)
+    }
+
+
     const editTrue = () => {
         setEdit(true)
     }
 
-    const editFalse = (event: KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
+    const editFalse = () => {
+        {
             setEdit(false)
         }
 
     }
     return (
         edit
-        ? <input value={props.title} onKeyPress={editFalse} autoFocus={true}/>
+        ? <input value={newTaskTitle}
+                 onClick={editFalse}
+                 autoFocus={true}
+                 onChange={onChangeHandler}/>
+
         : <div>
             <span onDoubleClick={editTrue} className={props.checked ? styles.isDone : ''}>{props.title}</span>
         </div>
