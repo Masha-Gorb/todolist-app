26f593bcb49aac96852ced0bdcecb053aed8a1d9
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-11-12 13:49:27 +0300

    forget to add id in AC

--- src/redux/TaskReducer.ts
+++ src/redux/TaskReducer.ts
@@ -1,11 +1,16 @@
 import {tasksPropsType} from "../TodoList";
 import {TodolistsType} from "../App";
+import {v1} from "uuid";
 
 export const TodolistReducer = (state: Array<TodolistsType>, action: generalType) => {
 
     switch (action.type) {
         case 'ADD-TODOLIST' : {
-            console.log('dzink!')
+            console.log(action.todolistId)
+
+            // let newTodolistID = v1();
+            // setTodolists([{id: newTodolistID, title: newTodoListTitle, filter: 'All'}, ...todolists])
+            // setTasks({...tasks, [newTodolistID]:[]})
             return state
         }
         default: return state
@@ -19,6 +24,7 @@ type AddTodolistACType = ReturnType<typeof AddTodolistAC>
 //создаем экшон обязательно пишет тип эз конст чтобы тайп был не стринга, а КОНКРЕТНО что написано
 export const AddTodolistAC = (todolistId: string, newTodoListTitle: string) => {
     return {
-        type: 'ADD-TODOLIST'
+        type: 'ADD-TODOLIST',
+        todolistId: todolistId
     } as const
 }
