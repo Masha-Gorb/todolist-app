8dc5d196ac6fb732fa9f13fea1a2b2f47fe8fb28
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-01-20 13:24:28 +0300

    refactor addItenForm

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -5,6 +5,7 @@ type AddItemFormPropsType = {
 }
 
 export function AddItemForm(props: AddItemFormPropsType) {
+    console.log('Add Item form component')
 
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
@@ -23,7 +24,9 @@ export function AddItemForm(props: AddItemFormPropsType) {
     }
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
+        if(error !== null) {
+            setError(null)
+        }
         if (e.charCode === 13) {
             addItem();
         }
